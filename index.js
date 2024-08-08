3ebf51094480a18a2a164076cdf4f881d53f2d62
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:26:27 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Get started!
+let name = prompt("what is your name?")
+console.log("welcome to my game, "+ name)
+let name = prompt("What's your name?")
+
+let role = prompt("What role do you want? (magician, fighter, peasant)")
+console.log(`Okay, ${name}, your role is now ${role}.`)
+
+let powerLevel = prompt("What is your power level?")
+
+powerLevel = parseInt(powerLevel)
